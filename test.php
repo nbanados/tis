81d935aaef5381f88ac71a3a0a17ee19e7666ccb
nbanados <nbanados@alumnos.ua.cl>
2015-08-19 19:25:13 -0400

    Se sube test

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+$mysql_host="54.68.135.137";
+$mysql_user="user"; 
+$mysql_password="pepito.P0";
+$link = mysql_connect($mysql_host, $mysql_user, $mysql_password)
+    or die('No se pudo conectar');
+echo 'Connected successfully';
+mysql_select_db('my_database') or die('No se pudo seleccionar la base de datos');
+?>
