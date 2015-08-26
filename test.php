cc0d3872bfed19bd67044e63278e3828e7005ba4
nbanados <nbanados@alumnos.uai.cl>
2015-08-26 20:14:30 -0300

    Update test.php

--- test.php
+++ test.php
@@ -1,11 +1,13 @@
 <?php
-$mysql_host="54.68.135.137";
-$mysql_user="user"; 
-$mysql_password="pepito.P0";
+
+
+$mysql_host=$_SERVER['host'];
+$mysql_user=$_SERVER['user']; 
+$mysql_password=$_SERVER['pass'];
 $link = mysql_connect($mysql_host, $mysql_user, $mysql_password)
     or die('No se pudo conectar');
 echo 'Connected successfully';
 mysql_select_db('my_database') or die('No se pudo seleccionar la base de datos');
 
-echo 'hola perro';
+echo 'hola precioso';
 ?>
